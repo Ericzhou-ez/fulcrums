2466710ed8a8d83297c0cbc52622c51adb060d38
Ericzhou-ez <zhouxiaozhoueric@gmail.com>
2025-02-13 08:02:26 -0800

    hero page + cool cta btn

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,25 +1,67 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 export default function Hero() {
    return (
-      <Typography
-         variant="h1"
-         component="h1"
-         align="center"
-         sx={{
-            fontSize: {
-               xs: "3.4rem", 
-               sm: "5.5rem", 
-               md: "6.2rem", 
-               lg: "7rem", 
-            },
-            fontWeight: 700,
-            lineHeight: 1.2,
-            color: "text.primary",
-            margin: "0 40px",
-         }}
-      >
-         一键搞定<br></br>产品追踪,<br></br>报价, 报关.
-      </Typography>
+      <div className="hero">
+         <Typography
+            variant="h1"
+            component="h1"
+            align="center"
+            sx={{
+               fontSize: {
+                  xs: "3.4rem",
+                  sm: "4.5rem",
+                  md: "6.2rem",
+                  lg: "7rem",
+               },
+               fontWeight: 700,
+               lineHeight: 1.2,
+               color: "text.primary",
+               margin: "0 40px",
+               marginBottom: "50px",
+               letterSpacing: "0.05rem",
+            }}
+            className="hero-title"
+         >
+            产品追踪, 报价,<br></br>报关,{" "}
+            <span className="clipped-text">一键搞定!</span>
+         </Typography>
+
+         <Typography
+            variant="h2"
+            align="center"
+            sx={{
+               fontSize: {
+                  xs: "0.8rem",
+                  sm: "1rem",
+                  md: "1.2rem",
+                  lg: "1.5rem",
+               },
+               fontWeight: "600",
+               color: "text.secondary",
+               marginBottom: "30px",
+            }}
+            className="hero-h2-description"
+         >
+            BT-Assist 提供从产品采购到报关全流程管理.
+         </Typography>
+
+         <Stack
+            spacing={2}
+            direction="row"
+            justifyContent="center"
+            className="cta-buttons"
+            gap="10px"
+         >
+            <div className="animated-border">
+               <a href="/">
+                  <button className="cta-join-button-hero">理解更多</button>
+               </a>
+            </div>
+            <a href="/signin">
+               <button className="cta-login">登录</button>
+            </a>
+         </Stack>
+      </div>
    );
 }
