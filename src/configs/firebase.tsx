714ccbcfc267d27abb3ae053539f265953076a6d
Ericzhou-ez <zhouxiaozhoueric@gmail.com>
2025-02-21 16:47:56 -0800

    vercel env is crazzzy

--- src/configs/firebase.tsx
+++ src/configs/firebase.tsx
@@ -4,13 +4,34 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-   projectId: "fulcrums-ca",
-   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+   apiKey:
+      import.meta.env.VITE_FIREBASE_API_KEY ||
+      process.env.VITE_FIREBASE_API_KEY ||
+      "MISSING",
+   authDomain:
+      import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
+      process.env.VITE_FIREBASE_AUTH_DOMAIN ||
+      "MISSING",
+   projectId:
+      import.meta.env.VITE_FIREBASE_PROJECT_ID ||
+      process.env.VITE_FIREBASE_PROJECT_ID ||
+      "fulcrums-ca",
+   storageBucket:
+      import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
+      process.env.VITE_FIREBASE_STORAGE_BUCKET ||
+      "MISSING",
+   messagingSenderId:
+      import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
+      process.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
+      "MISSING",
+   appId:
+      import.meta.env.VITE_FIREBASE_APP_ID ||
+      process.env.VITE_FIREBASE_APP_ID ||
+      "MISSING",
+   measurementId:
+      import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ||
+      process.env.VITE_FIREBASE_MEASUREMENT_ID ||
+      "MISSING",
 };
 
 export const actionCodeSettings = {
@@ -24,10 +45,3 @@ export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleAuth = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
-console.log("🔥 Firebase Environment Variables:");
-console.log(
-   "VITE_FIREBASE_PROJECT_ID:",
-   import.meta.env.VITE_FIREBASE_PROJECT_ID
-);
-console.log("VITE_FIREBASE_API_KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
