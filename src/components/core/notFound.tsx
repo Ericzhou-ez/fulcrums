087d9c66af7b75435337ab5577bcd18fc8c1d92b
Ericzhou-ez <zhouxiaozhoueric@gmail.com>
2025-02-27 15:56:58 -0800

    dvh in lieu of vh for mobile

--- src/components/core/notFound.tsx
+++ src/components/core/notFound.tsx
@@ -14,14 +14,31 @@ export default function NotFound() {
 
          <div className={styles.info}>
             <h2>我们找不到该页面</h2>
-            <p>您访问的页面不存在，可能从未创建过。我们为此表示歉意。</p>
+            <p>您访问的页面不存在，可能从未创建过.</p>
             <a href="/">
-               <button style={{
-                  border: "none",
-                  backgroundColor: "rgb(230, 230, 230)",
-                  padding: "10px 18px",
-                  borderRadius: "20px",
-               }}>回到首页</button>
+               <button
+                  style={{
+                     border: "none",
+                     backgroundColor: "rgb(230, 230, 230)",
+                     padding: "10px 18px",
+                     borderRadius: "20px",
+                     marginRight: "10px",
+                  }}
+               >
+                  回到首页
+               </button>
+            </a>
+            <a href="/dashboard">
+               <button
+                  style={{
+                     border: "none",
+                     backgroundColor: "#adb0a0",
+                     padding: "10px 18px",
+                     borderRadius: "20px",
+                  }}
+               >
+                  回到表板
+               </button>
             </a>
          </div>
       </div>
